a97150741624d081249d3519afae2ecf869bf15a
Isaac Turner <turner.isaac@gmail.com>
2012-09-03 18:24:59 +0100

    Fixed ascii_code to use mac compatible switches; Added comments

--- profile.sh
+++ profile.sh
@@ -7,16 +7,19 @@ export PERL5LIB=${PERL5LIB}:${HOME}/perl/lib/
 # Aliases to common ssh
 alias myserver='ssh turner@myserver.com'
 
+# Set the variable $TAB to be the tab character
 export TAB=`echo -e "\t"`
 
 # A function for char -> ASCII
-function ascii_code { echo -n $1 | od -t u4 -N 1 | grep -m 1 -oP '[1-9]+\d*'; }
+function ascii_code { echo -n $1 | od -t u4 -N 1 | grep -m 1 -oE '[1-9]+\d*'; }
 
 # Convert number of bytes to human readable string (using Kb,Mb,Gb,Tb,Pb)
 alias mem2str='perl -w -e '"'"'my $x = $ARGV[0]; if(!defined($x) && !defined($x = <>)) { die("Cannot read in");} chomp($x); if($x !~ /^\d*\.?\d*(e[+-]\d*)?$/) { die("Not a number");} my @t = qw(B Kb Mb Gb Tb Pb); my $i=0; for($i=0; $x>=1024; $i++) { $x /= 1024;} printf(($i==0?$x:"%.2f")." $t[$i]\n",$x);'"'"
 
-# Get real memory used by all processes, sum it, convert from kb to bytes, print in human readable
+# Get virtual memory usage by all processes in human readable
 alias mem_vsize='ps -eo vsize | awk '"'"'{s+=$1} END {print s*1024}'"'"' | mem2str'
+# Get real memory usage by all processes in human readable
 alias mem_rss='ps -eo rss | awk '"'"'{s+=$1} END {print s*1024}'"'"' | mem2str'
+# On linux set this to mem_vsize, on mac mem_rss
 alias mem_used='mem_vsize'
-
+#alias mem_used='mem_rss'
