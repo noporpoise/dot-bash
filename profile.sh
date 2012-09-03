7e617202402c2386a722737590ce8601b447b215
Isaac Turner <turner.isaac@gmail.com>
2012-09-03 18:26:10 +0100

    Wrapped perl script

--- profile.sh
+++ profile.sh
@@ -14,7 +14,14 @@ export TAB=`echo -e "\t"`
 function ascii_code { echo -n $1 | od -t u4 -N 1 | grep -m 1 -oE '[1-9]+\d*'; }
 
 # Convert number of bytes to human readable string (using Kb,Mb,Gb,Tb,Pb)
-alias mem2str='perl -w -e '"'"'my $x = $ARGV[0]; if(!defined($x) && !defined($x = <>)) { die("Cannot read in");} chomp($x); if($x !~ /^\d*\.?\d*(e[+-]\d*)?$/) { die("Not a number");} my @t = qw(B Kb Mb Gb Tb Pb); my $i=0; for($i=0; $x>=1024; $i++) { $x /= 1024;} printf(($i==0?$x:"%.2f")." $t[$i]\n",$x);'"'"
+alias mem2str='perl -w -e '"'"'my $x = $ARGV[0];
+if(!defined($x) && !defined($x = <>)) { die("Cannot read in");}
+chomp($x);
+if($x !~ /^\d*\.?\d*(e[+-]\d*)?$/) { die("Not a number");}
+my @t = qw(B Kb Mb Gb Tb Pb);
+my $i=0;
+for($i=0; $x>=1024; $i++) { $x /= 1024;}
+printf(($i==0?$x:"%.2f")." $t[$i]\n",$x);'"'"
 
 # Get virtual memory usage by all processes in human readable
 alias mem_vsize='ps -eo vsize | awk '"'"'{s+=$1} END {print s*1024}'"'"' | mem2str'
