8420f109e161d3d52781a0d2711585acdee2e3ac
Isaac Turner <turner.isaac@gmail.com>
2012-09-05 14:09:30 +0100

    Added descriptors to the tops of files, moved code between files

--- bashrc.sh
+++ bashrc.sh
@@ -1,3 +1,11 @@
+#
+# Examples to add to your ~/.bashrc or ~/.screenrc files
+# Includes all 'alias' and 'function' definitions
+#
+
+# Aliases to common ssh
+alias myserver='ssh turner@myserver.com'
+
 # A function for char -> ASCII
 function ascii_code { echo -n $1 | od -t u4 -N 1 | grep -m 1 -oE '[1-9]+\d*'; }
 
--- profile.sh
+++ profile.sh
@@ -1,14 +1,13 @@
+#
+# Examples to add to your ~/.profile or ~/.bash_profile files
+# Includes all exported variable definitions
+#
+
 # Add to your path
 export PATH=$PATH:~/bin/
 
 # Add to your perl lib path
 export PERL5LIB=${PERL5LIB}:${HOME}/perl/lib/
 
-# Aliases to common ssh
-alias myserver='ssh turner@myserver.com'
-
 # Set the variable $TAB to be the tab character
 export TAB=`echo -e "\t"`
-
-# A function for char -> ASCII
-function ascii_code { echo -n $1 | od -t u4 -N 1 | grep -m 1 -oE '[1-9]+\d*'; }
